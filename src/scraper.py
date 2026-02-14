d56e42ff33235830a47686a128c9d7f09c616ce8
José Isaac <joseisaacnascimento@gmail.com>
2026-02-14 19:46:31 -0300

    feat(scrapper): implement pagination detection

--- src/scraper.py
+++ src/scraper.py
@@ -4,4 +4,8 @@ from selenium.webdriver.remote.webdriver import WebDriver
 def open_link(browser: WebDriver, url: str)-> None:
     browser.get(url)
     
-    
+def get_total_pages(browser: WebDriver)-> int:
+    pagination_elements = browser.find_elements(By.CLASS_NAME, "MuiPaginationItem-page")
+    pages = [int(el.text) for el in pagination_elements if el.text.isdigit()]
+    print(f"Foram encontradas {max(pages)} páginas" )
+    return max(pages) if pages else 1
