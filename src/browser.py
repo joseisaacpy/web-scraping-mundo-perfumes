1c6c73e1483c16c55ee99efaa77ab00ed7f6cff1
José Isaac <joseisaacnascimento@gmail.com>
2026-02-15 14:53:02 -0300

    feat(browser): maximize browser window on startup

--- src/browser.py
+++ src/browser.py
@@ -3,7 +3,9 @@ from selenium import webdriver
 
 def init_browser():
     browser = webdriver.Chrome()
+    browser.maximize_window()
     return browser
+
 
 def close_browser(browser):
     browser.quit()
